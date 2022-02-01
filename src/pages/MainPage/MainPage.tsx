7bb8c05aaa6ee04fc28c3b660aac107a00e0ffa5
Oleksii Skopiuk <oskop@softserveinc.com>
2022-02-01 19:23:13 +0200

    Add condition for render title on Main, amend mock to be more polite

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,20 @@
 import { useAppSelector } from 'app/hooks';
+import { selectWords } from 'features/inputForm/inputSlice';
 import { Layout } from 'features/layout/Layout';
 import { NavBar } from 'features/navBar/NavBar';
 import styles from './MainPage.module.css';
 
 export const MainPage = () => {
-  const words = useAppSelector((state) => state.wordPairs.words);
+  const words = useAppSelector(selectWords);
 
   return (
     <Layout>
       <NavBar />
-      <span className={styles.title}>Your learned words: </span>
+      {words.length ? (
+        <span className={styles.title}>Your learned words: </span>
+      ) : (
+        <span className={styles.title}>You don't have saved words yet</span>
+      )}
       {words.map((word) => (
         <li key={word.value} className={styles.listItem}>
           <span className={styles.value}>{word.value}</span>
